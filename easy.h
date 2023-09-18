758c0a7a01f3eac1c23543f0d282b00a62cb1e11
CectAss <78093532+CectAss@users.noreply.github.com>
2023-09-18 09:54:15 +0300

    Update easy.h

--- easy.h
+++ easy.h
@@ -9,7 +9,7 @@ int itc_str(int a, int b, int c);
 double itc_scir(int a);
 
 int itc_abs(int a);
-double itc_fabs(int a);
+double itc_fabs(double a);
 int itc_revnbr(int a);
 bool itc_iseven(int a);
 
